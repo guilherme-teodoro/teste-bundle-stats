204f9daec86a7a8a2db49820d356d2a3f26405aa
Guilherme Teodoro <guilherme.m.teodoro@gmail.com>
2023-09-29 11:26:58 -0300

    chore: add date-fns

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { format } from "date-fns";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {format(new Date(Date.now()), "dd-mm-yyyy")}
         <a
           className="App-link"
           href="https://reactjs.org"
