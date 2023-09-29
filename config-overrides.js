c01a909dbd57d151a351c08c7af767ee605827c9
Guilherme Teodoro <guilherme.m.teodoro@gmail.com>
2023-09-29 15:25:15 -0300

    change react version

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,13 @@
 // config-overrides.js
+//
+const path = require("path");
 const { override, addWebpackPlugin } = require("customize-cra");
 const { BundleStatsWebpackPlugin } = require("bundle-stats-webpack-plugin");
 
 module.exports = override(
   addWebpackPlugin(
     new BundleStatsWebpackPlugin({
-      baseline: !process.env.COMPARE,
-      compare: process.env.COMPARE,
+      baselineFilepath: path.resolve(__dirname, "."),
     }),
   ),
 );
