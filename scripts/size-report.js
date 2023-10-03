a68573b751324afc3f20a538ec6ea134486ff5a9
Guilherme Teodoro <guilherme.m.teodoro@gmail.com>
2023-10-03 15:30:27 -0300

    gfdjgfdjgfd

--- scripts/size-report.js
+++ scripts/size-report.js
@@ -1,26 +1,34 @@
+const { createAppAuth } = require("@octokit/auth-app");
+const { Octokit, App } = require("octokit");
+
 require("dotenv").config();
-const jwt = require("jsonwebtoken");
-const axios = require("axios");
 
-const token = jwt.sign({}, process.env.PRIVATE_KEY, {
-  algorithm: "RS256",
-  expiresIn: "10m",
-  issuer: process.env.APP_ID,
-});
+// CIRCLE_PR_NUMBER
 
-console.log(token);
+async function main() {
+  const octokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+      appId: process.env.APP_ID,
+      privateKey: process.env.PRIVATE_KEY,
+      installationId: process.env.INSTALLATION_ID,
+    },
+  });
 
-function getAccessToken() {
-  return axios.post(
-    `https://api.github.com/app/installations/${process.env.INSTALLATION_ID}/access_tokens`,
+  // authenticates as app based on request URLs
+  await octokit.rest.apps.getAuthenticated();
+
+  // creates an installation access token as needed
+  // assumes that installationId 123 belongs to @octocat, otherwise the request will fail
+  await octokit.request(
+    "PUT /repos/guilherme-teodoro/teste-bundle-stats/pulls/2/reviews/1651441012",
     {
+      body: "Great stuff! 2",
       headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${token}`,
         "X-GitHub-Api-Version": "2022-11-28",
       },
     },
   );
 }
 
-getAccessToken().then(({ data }) => {});
+main();
