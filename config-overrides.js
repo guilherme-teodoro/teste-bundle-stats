900ae5033c47a18652f7ebcfcb5294572b78fbd0
Guilherme Teodoro <guilherme.m.teodoro@gmail.com>
2023-10-05 10:30:40 -0300

    final

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,11 @@
-// config-overrides.js
-//
-const path = require("path");
 const { override, addWebpackPlugin } = require("customize-cra");
 const { BundleStatsWebpackPlugin } = require("bundle-stats-webpack-plugin");
 
 module.exports = override(
   addWebpackPlugin(
     new BundleStatsWebpackPlugin({
-      baselineFilepath: "../stats.json",
+      json: true,
+      html: false,
     }),
   ),
 );
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import { format } from "date-fns";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{format(new Date(), "dd")}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
